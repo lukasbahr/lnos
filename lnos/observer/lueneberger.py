58a923f22b667de343b438ed4079f3269a18a017
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2021-05-26 16:32:48 +0200

    updated comments

--- lnos/observer/lueneberger.py
+++ lnos/observer/lueneberger.py
@@ -1,11 +1,26 @@
 import numpy as np
-from scipy import linalg, integrate, interpolate
+from scipy import linalg
 from torchdiffeq import odeint
 import torch
 
-class LuenebergerObserver():
 
+class LuenebergerObserver():
     def __init__(self, dim_x: int, dim_y: int, f: callable, g: callable, h: callable, u: callable):
+        """
+        Constructor for setting the dynamics of the Luenberger Observer. 
+        Also constructs placeholder for D and F matrices.
+
+        Arguments:
+            dim_x -- dimension of states
+            dim_y -- dimension of inputs
+            f -- callable function for system dynamics
+            g -- callable function for control input
+            h -- callable function for measurement 
+            u -- callable function for input
+
+        Returns:
+            None
+        """
         self.dim_x = dim_x
         self.dim_y = dim_y
         self.dim_z = self.dim_y * (self.dim_x + 1)
@@ -21,8 +36,14 @@ class LuenebergerObserver():
 
     def tensorDFromEigen(self, eigen: torch.tensor) -> torch.tensor:
         """
-        Generates the state matrix D to use in a Luenberger
-        observer given the desired eigenvalues.
+        Return matrix D as conjugate block matrix from eigenvectors 
+        in form of conjugate complex numbers. 
+
+        Arguments:
+            eigen -- dimension of states
+
+        Returns:
+            D -- conjugate block matrix
         """
         self.eigenD = eigen
         eig_complex, eig_real = [x for x in eigen if x.imag != 0], [
@@ -33,7 +54,7 @@ class LuenebergerObserver():
             eigenCell = self.eigenCellFromEigen(eig_complex, eig_real)
             D = linalg.block_diag(*eigenCell[:])
 
-            return torch.tensor(D,dtype=torch.float32)
+            return torch.tensor(D, dtype=torch.float32)
 
     @staticmethod
     def eigenCellFromEigen(eig_complex: torch.tensor, eig_real: torch.tensor) -> []:
@@ -41,6 +62,12 @@ class LuenebergerObserver():
         Generates a cell array containing 2X2 real
         matrices for each pair of complex conjugate eigenvalues passed in the
         arguments, and real scalar for each real eigenvalue.
+
+        Arguments:
+            eigen -- dimension of states
+
+        Returns:
+            array -- array of conjugate pairs of eigenvectors
         """
         eigenCell = []
 
@@ -64,7 +91,17 @@ class LuenebergerObserver():
         Runs and outputs the results from 
         multiple simulations of an input-affine nonlinear system driving a 
         Luenberger observer target system.
+
+        Arguments:
+            y_0 -- initial value
+            tsim -- tuple of (start,end)
+            dt -- step width
+
+        Returns:
+            tq -- array timesteps
+            sol -- solver solution for x_dot and z_dot
         """
+
         def dydt(t, y):
             y = y.float()
             x = y[0:self.dim_x]
