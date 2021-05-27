da26f4b0a6c085e78cbf8fce077eab94ada14b80
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2021-05-27 14:32:42 +0200

    updates setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name='Measurements',
-    url='https://github.com/jladan/package_demo',
-    author='John Ladan',
-    author_email='jladan@uwaterloo.ca',
-    # Needed to actually package something
+    name='lnos',
+    url='https://github.com/lukasbahr/lnos',
+    author='Lukas Bahr',
     packages=['lnos.net', 'lnos.experiments', 'lnos.datasets', 'lnos.observer'],
-    # Needed for dependencies
-    install_requires=['numpy', 'torch', 'scipy', 'matplotlib'],
-    # *strongly* suggested for sharing
-    version='0.1',
-    # The license can be anything you like
+    install_requires=['numpy', 'torch', 'scipy', 'matplotlib', 'torchdiffeq'],
+    version='0.01',
     license='MIT',
-    description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    description='Implemantation of lnos.',
 )
