c3d69a6c44ed846461f71ae63975b8eeecdbcf7f
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2021-05-27 12:21:30 +0200

    update setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['lnos'],
     # Needed for dependencies
-    install_requires=['numpy','torch','math','scipy','matplotlib'],
+    install_requires=['numpy','torch','scipy','matplotlib'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
